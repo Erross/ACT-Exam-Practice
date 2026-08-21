3e77761f778a058f30d8f1ee63213f5e2fbee8a5
Erross <33665337+Erross@users.noreply.github.com>
2026-08-21 13:27:46 -0500

    Apply Reading distractor curation to effective bank

--- data/reading.js
+++ data/reading.js
@@ -1,9 +1,12 @@
 import { READING_LITERARY_PASSAGES } from './reading/literary.js';
 import { READING_INFORMATIONAL_SINGLE_PASSAGES } from './reading/informational-single.js';
 import { READING_INFORMATIONAL_MULTI_PASSAGES } from './reading/informational-multi.js';
+import { applyReadingChoiceRepairs } from './reading-choice-repairs.js';
 
-export const READING_PASSAGES = Object.freeze([
+const RAW_READING_PASSAGES = [
   ...READING_LITERARY_PASSAGES,
   ...READING_INFORMATIONAL_SINGLE_PASSAGES,
   ...READING_INFORMATIONAL_MULTI_PASSAGES,
-]);
+];
+
+export const READING_PASSAGES = Object.freeze(applyReadingChoiceRepairs(RAW_READING_PASSAGES));
