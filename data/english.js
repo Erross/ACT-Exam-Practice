e8244093dd8ee68d97d3c526c11400e2e76b6f4d
Erross <33665337+Erross@users.noreply.github.com>
2026-08-21 12:44:36 -0500

    Assemble initial English passage bank

---
+++ data/english.js
@@ -0,0 +1,11 @@
+import { ENGLISH_LONG_A } from './english/long-a.js';
+import { ENGLISH_LONG_B } from './english/long-b.js';
+import { ENGLISH_SHORT_A } from './english/short-a.js';
+import { ENGLISH_SHORT_B } from './english/short-b.js';
+
+export const ENGLISH_PASSAGES = Object.freeze([
+  ...ENGLISH_LONG_A,
+  ...ENGLISH_LONG_B,
+  ...ENGLISH_SHORT_A,
+  ...ENGLISH_SHORT_B,
+]);
