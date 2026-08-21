f6fb4a9d83956bc623fbd7e03a320cfdc088ae25
Erross <33665337+Erross@users.noreply.github.com>
2026-08-21 14:43:03 -0500

    Wire Science expansion to double draft bank

--- data/science.js
+++ data/science.js
@@ -1,10 +1,16 @@
 import { SCIENCE_DATA_REPRESENTATION_SETS } from './science/data-representation.js';
 import { SCIENCE_RESEARCH_SUMMARY_SETS } from './science/research-summaries.js';
 import { SCIENCE_CONFLICTING_VIEWPOINT_SETS } from './science/conflicting-viewpoints.js';
+import { SCIENCE_EXPANSION_DATA_REPRESENTATION_SETS } from './science/expansion-data-representation.js';
+import { SCIENCE_EXPANSION_RESEARCH_SUMMARY_SETS } from './science/expansion-research-summaries.js';
+import { SCIENCE_EXPANSION_CONFLICTING_VIEWPOINT_SETS } from './science/expansion-conflicting-viewpoints.js';
 import { applyScienceChoiceRepairs } from './science-choice-repairs.js';
 
 export const SCIENCE_SETS = Object.freeze(applyScienceChoiceRepairs([
   ...SCIENCE_DATA_REPRESENTATION_SETS,
   ...SCIENCE_RESEARCH_SUMMARY_SETS,
   ...SCIENCE_CONFLICTING_VIEWPOINT_SETS,
+  ...SCIENCE_EXPANSION_DATA_REPRESENTATION_SETS,
+  ...SCIENCE_EXPANSION_RESEARCH_SUMMARY_SETS,
+  ...SCIENCE_EXPANSION_CONFLICTING_VIEWPOINT_SETS,
 ]));
