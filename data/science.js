8c278076897b50a2517d75b2813c83f1a84d993f
Erross <33665337+Erross@users.noreply.github.com>
2026-08-21 14:37:16 -0500

    Wire Science distractor repairs into browser-effective bank

--- data/science.js
+++ data/science.js
@@ -1,9 +1,10 @@
 import { SCIENCE_DATA_REPRESENTATION_SETS } from './science/data-representation.js';
 import { SCIENCE_RESEARCH_SUMMARY_SETS } from './science/research-summaries.js';
 import { SCIENCE_CONFLICTING_VIEWPOINT_SETS } from './science/conflicting-viewpoints.js';
+import { applyScienceChoiceRepairs } from './science-choice-repairs.js';
 
-export const SCIENCE_SETS = Object.freeze([
+export const SCIENCE_SETS = Object.freeze(applyScienceChoiceRepairs([
   ...SCIENCE_DATA_REPRESENTATION_SETS,
   ...SCIENCE_RESEARCH_SUMMARY_SETS,
   ...SCIENCE_CONFLICTING_VIEWPOINT_SETS,
-]);
+]));
